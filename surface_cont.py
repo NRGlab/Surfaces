e50bc51506899121bae63e952db7b9845fb20fa7
nataliateruel <56839341+nataliateruel@users.noreply.github.com>
2023-02-21 13:45:01 -0500

    Add files via upload

--- surface_cont.py
+++ surface_cont.py
@@ -7,7 +7,6 @@ import re
 import pandas as pd
 import itertools
 import time
-import steric_clashes
 
 #Useful dicts
 aa = {'C':'CYS', 'D':'ASP', 'S':'SER', 'Q':'GLN', 'K':'LYS', 'I':'ILE', 'P':'PRO', 'T':'THR', 'F':'PHE', 'N':'ASN', 'G':'GLY', 'H':'HIS', 'L':'LEU', 'R':'ARG', 'W':'TRP', 'A':'ALA', 'V':'VAL', 'E':'GLU', 'Y':'TYR', 'M':'MET'}
@@ -44,12 +43,13 @@ def read_residues(pdb_file, chain1, chain2):
     return (list_chain1, list_chain2, chains, atoms_numbers)
 
 def clean_pdb(pdb_file, chain1, chain2, new_file):
+    chains = chain1 + chain2
     f = open(pdb_file, 'r')
     Lines1 = f.readlines()
     Lines2 = []
     for line in Lines1:
         if line[:4] == 'ATOM':
-            if line[21] == chain1 or line[21] == chain2:
+            if line[21] in chain2:
                 #print (line)
                 Lines2.append(line)
     f.close()
@@ -60,52 +60,28 @@ def clean_pdb(pdb_file, chain1, chain2, new_file):
     return
 
 #Function to generate the file with the output of perl code vcont.pl
-def vcont(pdb_name):
-    string = "perl -w vcont.pl -f " + pdb_name + " > vcont_output.txt"
-    #print (string)
-    os.system(string)
-    #print ('vcont done')
 
 def vcon(pdb_name):
     string = "./vcon " + pdb_name + " > vcon_file.txt"
-    #print (string)
     os.system(string)
-    #print ('vcon done')
-
 
 #Functions to fix the names of the chains
 
-def test_chain(atom, chain1, chain2, atoms_numbers):
-    chains = chain1 + chain2
-    for i in range(len(chains)):
-        if atom in atoms_numbers[i]:
-            chain = chains[i]
-            return (chain)
-    return (False)
+def get_chain(atom, og_chain, chains, atom_numbers):
+    if og_chain in chains:
+        index = chains.index(og_chain)
+    else:
+        return (og_chain)
+    if atom in atom_numbers[index]:
+        return (og_chain)
+    else:
+        for i in range(len(chains)):
+            if i != index:
+                if atom in atom_numbers[i]:
+                    return (chains[i])
+    return (0)
 
-def fix_chain(file, chain1, chain2, atoms_numbers):
-    f = open(file, 'r')
-    Lines1 = f.readlines()
-    Lines2 = []
-    for line in Lines1:
-        if line[:1] != '#' and line[31:34] != 'Sol' and line != '\n':
-            chain = test_chain(int(line[20:30]), chain1, chain2, atoms_numbers)
-            if not chain:
-                Lines2.append(line)
-            else:
-                line = line[:45] + chain + line[46:]
-                Lines2.append(line)
-        else:
-            Lines2.append(line)
-    f.close()
-    e = open(file, 'w')
-    for line in Lines2:
-        e.write(line)
-    e.close()
-    return
-
-
-#Functions to open vcon results, AMINO.def and MC_st0r5.2_6.dat for atom types interactions
+#Functions to open vcon results, .def and .dat for atom types interactions
 
 def read_atom(line):
     atnum = int(line[:6])
@@ -113,47 +89,40 @@ def read_atom(line):
     resnum = int(line[12:17])
     res = line[19:22]
     chain = line[23:24]
-    #print (atnum)
-    #print (attype)
-    #print (resnum)
-    #print (res)
-    #print (chain)
     return (atnum,attype,resnum,res,chain)
 
 def read_surface(line):
     surf = (float(line[-6:-1]))
-    #print (surf)
     return (surf)
 
-def read_interactions(file, matrix, chain1, chain2, def_file, dat_file):
+def read_interactions(file, matrix, chain1, chain2, def_file, dat_file, atom_numbers):
+    chains = chain1 + chain2
     f = open(file, 'r')
     Lines = f.readlines()
     for line in Lines:
         if line[:1] != '#' and line != '\n':
             if line[31:34] == 'Sol':
-                #print (line)
                 main_line = line
                 main_atnum,main_attype,main_resnum,main_res,main_chain = read_atom(main_line)
-                main_residue = main_res+str(main_resnum)+main_chain
+                fixed_main_chain = get_chain(main_atnum,main_chain,chains,atom_numbers)
+                if fixed_main_chain != 0:
+                    main_residue = main_res+str(main_resnum)+fixed_main_chain
             else:
-                #print (line[22:])
                 atnum,attype,resnum,res,chain = read_atom(line[22:])
-                other_residue = res+str(resnum)+chain
-                surf = read_surface(line)
-                if (chain in chain1 and main_chain in chain2) or (chain in chain2 and main_chain in chain1):
-                    if main_chain in chain2:
-                        #print (other_residue, main_residue)
-                        matrix.loc[other_residue, main_residue] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file))/2
-                    if main_chain in chain1:
-                        #print (main_residue, other_residue)
-                        matrix.loc[main_residue, other_residue] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file))/2
+                fixed_other_chain = get_chain(atnum,chain,chains,atom_numbers)
+                if fixed_other_chain != 0:
+                    other_residue = res+str(resnum)+fixed_other_chain
+                    surf = read_surface(line)
+                    if (fixed_other_chain in chain1 and fixed_main_chain in chain2) or (fixed_other_chain in chain2 and fixed_main_chain in chain1):
+                        if fixed_main_chain in chain2:
+                            matrix.loc[other_residue, main_residue] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file))/2
+                        if fixed_main_chain in chain1:
+                            matrix.loc[main_residue, other_residue] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file))/2
   
     return(matrix)
 
-
 #get the atom type number from def file of choice
 def atomtype_num(def_file, res, attyp):
-    # solve 'OXT' issue
     attyp = attyp.replace(" ", "")
     f = open(def_file, 'r')
     Lines = f.readlines()
@@ -190,57 +159,6 @@ def score(attype1, res1, attype2, res2, def_file, dat_file):
     value = interactions(dat_file, at1, at2)
     return (value)
 
-#Function to convert sequence in residue names
-
-def seq_to_res(seq, init_posit, dict, chain):
-    residues = []
-    for res in seq:
-        residues.append(dict[res] + str(init_posit) + chain)
-        init_posit = init_posit + 1
-    return (residues)
-
-#Functions related to interaction with solvent
-
-def create_unbound(pdb_file, chain, new_pdb_file):
-    f = open(pdb_file, 'r')
-    Lines = f.readlines()
-    new_Lines = []
-    for line in Lines:
-        if line[:4] == 'ATOM' and line[21] == chain:
-                new_Lines.append(line)
-    f.close()
-    e = open(new_pdb_file, 'w') 
-    for line in new_Lines: 
-        e.write(line)          
-    e.close()
-    return
-
-def solvent_score(def_file, dat_file, attype, res):
-    at1 = atomtype_num(def_file, res, attype)
-    at2 = 40
-    value = interactions(dat_file, at1, at2)
-    return (value)
-
-def read_solvent_surface(line):
-    ind = line.index('\n')
-    surf = (float(line[50:50+ind]))
-    return (surf)
-
-def interactions_solvent(def_file, dat_file, residues_list, file):
-    lista = [0]*len(residues_list)
-    f = open(file, 'r')
-    Lines = f.readlines()
-    for line in Lines:
-        if len(line) >=6 and line[35:38] == 'Sol':
-            surface = read_solvent_surface(line)
-            atnum,attype,resnum,res,chain = read_atom(line)
-            residue = res+str(resnum)+chain
-            if residue in residues_list:
-                score = solvent_score(def_file, dat_file, attype, res)
-                ind = residues_list.index(residue)
-                lista[ind] = lista[ind] + (score*surface)
-    return(lista)
-
 
 def main():
     
@@ -253,53 +171,23 @@ def main():
     parser.add_argument("-dat","--atomtypes_interactions", action="store")
     args=parser.parse_args()
     
-    steric_clashes.get_steric_clashes(args.pdb_file)
-
     res1, res2, chains, atom_numbers = read_residues(args.pdb_file, args.chain1, args.chain2)
     #print (res1, res2)
     #print (chains, atom_numbers)
     #clean_pdb(args.pdb_file, args.chain1, args.chain2, 'clean.pdb')
         
     vcon(args.pdb_file)
-    fix_chain('vcon_file.txt', args.chain1, args.chain2, atom_numbers)
   
     matrix = [ [ 0 for i in range(len(res2)) ] for j in range(len(res1)) ]
     matrix = pd.DataFrame(matrix)
     matrix.columns = res2
     matrix.index = res1
     
-    matrix = read_interactions('vcon_file.txt', matrix, args.chain1, args.chain2, args.atomtypes_definition, args.atomtypes_interactions)
-    
-    #print (matrix)
-    
+    matrix = read_interactions('vcon_file.txt', matrix, args.chain1, args.chain2, args.atomtypes_definition, args.atomtypes_interactions, atom_numbers)
+        
     matrix.to_csv(args.output_name)
     
     os.remove('vcon_file.txt')
-    
-    #TESTS
-    
-    #####TEST FIX CHAIN FUNCION#####
-    #fix_chain('test_file.txt')
-    
-    #####TEST READ ATOM FUNCTION#####
-    #f = open('test_file.txt', 'r')
-    #Lines = f.readlines()
-    #for line in Lines:
-    #    if line[:1] != '#' and line[6:7] == '|':
-    #        print(read_atom(line))
-              
-    #####TEST INTERACTIONS TAKEN FROM FLEXAID FILE#####
-    #interact1 = interactions('MC_st0r5.2_6.dat', 7, 14)
-    #interact2 = interactions('MC_st0r5.2_6.dat', 7, 18)
-    #print(interact1*interact2)
-    
-    #####TEST ATOM TYPE NUMBERS TAKEN FROM FLEXAID FILE#####
-    #print (atomtype_num('AMINO.def', 'THR', 'OG1'))
-    
-    #####TEST ITEMS ON THE MATRIX#####
-    #print(matrix.loc['PHE338F', 'THR27D'])
-    #matrix.loc['PHE338F', 'THR27D'] += 1
-    #print (matrix)
     
     return
     
--- surface_cont_lig.py
+++ surface_cont_lig.py
@@ -7,7 +7,6 @@ import re
 import pandas as pd
 import itertools
 import time
-#import steric_clashes
 
 #Useful dicts
 aa = {'C':'CYS', 'D':'ASP', 'S':'SER', 'Q':'GLN', 'K':'LYS', 'I':'ILE', 'P':'PRO', 'T':'THR', 'F':'PHE', 'N':'ASN', 'G':'GLY', 'H':'HIS', 'L':'LEU', 'R':'ARG', 'W':'TRP', 'A':'ALA', 'V':'VAL', 'E':'GLU', 'Y':'TYR', 'M':'MET'}
@@ -41,13 +40,13 @@ def read_residues(pdb_file, chains, ligand):
                 atoms_numbers[i].append(int(line[6:12]))
     return (list_chains, list_atoms, atoms_numbers)
 
-def clean_pdb(pdb_file, chain1, chain2, new_file):
+def clean_pdb(pdb_file, chains, new_file):
     f = open(pdb_file, 'r')
     Lines1 = f.readlines()
     Lines2 = []
     for line in Lines1:
         if line[:4] == 'ATOM':
-            if line[21] == chain1 or line[21] == chain2:
+            if line[21] in chains:
                 Lines2.append(line)
     f.close()
     e = open(new_file, 'w')
@@ -57,11 +56,6 @@ def clean_pdb(pdb_file, chain1, chain2, new_file):
     return
 
 #Function to generate the file with the output of perl code vcont.pl
-def vcont(pdb_name):
-    string = "perl -w vcont.pl -f " + pdb_name + " > vcont_output.txt"
-    #print (string)
-    os.system(string)
-    #print ('vcont done')
 
 def vcon(pdb_name):
     string = "./vcon " + pdb_name + " > vcon_file.txt"
@@ -72,34 +66,19 @@ def vcon(pdb_name):
 
 #Functions to fix the names of the chains
 
-def test_chain(atom, chains, atoms_numbers):
-    for i in range(len(chains)):
-        if atom in atoms_numbers[i]:
-            chain = chains[i]
-            return (chain)
-    return (False)
-
-def fix_chain(file, chains, atoms_numbers):
-    f = open(file, 'r')
-    Lines1 = f.readlines()
-    Lines2 = []
-    for line in Lines1:
-        if line[:1] != '#' and line[31:34] != 'Sol' and line != '\n':
-            chain = test_chain(int(line[20:29]), chains, atoms_numbers)
-            if not chain:
-                Lines2.append(line)
-            else:
-                line = line[:45] + chain + line[46:]
-                Lines2.append(line)
-        else:
-            Lines2.append(line)
-    f.close()
-    e = open(file, 'w')
-    for line in Lines2:
-        e.write(line)
-    e.close()
-    return
-
+def get_chain(atom, og_chain, chains, atom_numbers):
+    if og_chain in chains:
+        index = chains.index(og_chain)
+    else:
+        return (og_chain)
+    if atom in atom_numbers[index]:
+        return (og_chain)
+    else:
+        for i in range(len(chains)):
+            if i != index:
+                if atom in atom_numbers[i]:
+                    return (chains[i])
+    return (0)
 
 #Functions to open vcon results, AMINO.def and MC_st0r5.2_6.dat for atom types interactions
 
@@ -109,44 +88,37 @@ def read_atom(line):
     resnum = int(line[12:17])
     res = line[19:22]
     chain = line[23:24]
-    #print (atnum)
-    #print (attype)
-    #print (resnum)
-    #print (res)
-    #print (chain)
     return (atnum,attype,resnum,res,chain)
 
 def read_surface(line):
     surf = (float(line[-6:-1]))
-    #print (surf)
     return (surf)
 
-def read_interactions(file, matrix, chains, ligand, def_file, dat_file):
+def read_interactions(file, matrix, chains, ligand, def_file, dat_file, atom_numbers):
     f = open(file, 'r')
     Lines = f.readlines()
     for line in Lines:
         if line[:1] != '#' and line != '\n':
             if line[31:34] == 'Sol':
                 atnum,attype,resnum,res,chain = read_atom(line)
+                fixed_chain = get_chain(atnum,chain,chains,atom_numbers)
                 if res == ligand:
                     atom_name = str(atnum) + attype
-                    #print ("ATOM", atom_name)
             else:
                 main_atnum,main_attype,main_resnum,main_res,main_chain = read_atom(line[22:])
-                if main_res != ligand:
-                    main_residue = main_res+str(main_resnum)+main_chain
-                    surf = read_surface(line)
-                    #print (main_residue)
-                    if (main_chain in chains) and (res == ligand):
-                        #print (main_attype, main_res, attype, res)
-                        matrix.loc[main_residue, atom_name] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file))
+                fixed_main_chain = get_chain(main_atnum,main_chain,chains,atom_numbers)
+                if fixed_main_chain != 0:
+                    if main_res != ligand:
+                        main_residue = main_res+str(main_resnum)+fixed_main_chain
+                        surf = read_surface(line)
+                        if (fixed_main_chain in chains) and (res == ligand):
+                            matrix.loc[main_residue, atom_name] += (surf * score(main_attype, main_res, attype, res, def_file, dat_file))
   
     return(matrix)
 
 
 #get the atom type number from def file of choice
 def atomtype_num(def_file, res, attyp):
-    # solve 'OXT' issue
     attyp = attyp.replace(" ", "")
     f = open(def_file, 'r')
     Lines = f.readlines()
@@ -183,57 +155,6 @@ def score(attype1, res1, attype2, res2, def_file, dat_file):
     value = interactions(dat_file, at1, at2)
     return (value)
 
-#Function to convert sequence in residue names
-
-def seq_to_res(seq, init_posit, dict, chain):
-    residues = []
-    for res in seq:
-        residues.append(dict[res] + str(init_posit) + chain)
-        init_posit = init_posit + 1
-    return (residues)
-
-#Functions related to interaction with solvent
-
-def create_unbound(pdb_file, chain, new_pdb_file):
-    f = open(pdb_file, 'r')
-    Lines = f.readlines()
-    new_Lines = []
-    for line in Lines:
-        if line[:4] == 'ATOM' and line[21] == chain:
-                new_Lines.append(line)
-    f.close()
-    e = open(new_pdb_file, 'w') 
-    for line in new_Lines: 
-        e.write(line)          
-    e.close()
-    return
-
-def solvent_score(def_file, dat_file, attype, res):
-    at1 = atomtype_num(def_file, res, attype)
-    at2 = 40
-    value = interactions(dat_file, at1, at2)
-    return (value)
-
-def read_solvent_surface(line):
-    ind = line.index('\n')
-    surf = (float(line[50:50+ind]))
-    return (surf)
-
-def interactions_solvent(def_file, dat_file, residues_list, file):
-    lista = [0]*len(residues_list)
-    f = open(file, 'r')
-    Lines = f.readlines()
-    for line in Lines:
-        if len(line) >=6 and line[35:38] == 'Sol':
-            surface = read_solvent_surface(line)
-            atnum,attype,resnum,res,chain = read_atom(line)
-            residue = res+str(resnum)+chain
-            if residue in residues_list:
-                score = solvent_score(def_file, dat_file, attype, res)
-                ind = residues_list.index(residue)
-                lista[ind] = lista[ind] + (score*surface)
-    return(lista)
-
 
 def main():
     
@@ -246,57 +167,27 @@ def main():
     parser.add_argument("-dat","--atomtypes_interactions", action="store")
     args=parser.parse_args()
     
-    #steric_clashes.get_steric_clashes(args.pdb_file)
-
     res, atoms, atom_numbers = read_residues(args.pdb_file, args.chains, args.ligand)
-    
     #print (res, atoms)
     #print (args.chains, atom_numbers)
-    #clean_pdb(args.pdb_file, args.chain1, args.chain2, 'clean.pdb')
+    #clean_pdb(args.pdb_file, args.chains, 'clean.pdb')
     
     vcon(args.pdb_file)
-    fix_chain('vcon_file.txt', args.chains, atom_numbers)
+    #fix_chain('vcon_file.txt', args.chains, atom_numbers)
   
     matrix = [ [ 0 for i in range(len(atoms)) ] for j in range(len(res)) ]
     matrix = pd.DataFrame(matrix)
     matrix.columns = atoms
     matrix.index = res
     
-    matrix = read_interactions('vcon_file.txt', matrix, args.chains, args.ligand, args.atomtypes_definition, args.atomtypes_interactions)
-    
-    #print (matrix)
-    
+    matrix = read_interactions('vcon_file.txt', matrix, args.chains, args.ligand, args.atomtypes_definition, args.atomtypes_interactions, atom_numbers)
+        
     matrix.to_csv(args.output_name)
     
     # remove files
     os.remove("LIG.pdb")
     os.remove("LIG.mol2")
     os.remove("vcon_file.txt")
-    
-    #TESTS
-    
-    #####TEST FIX CHAIN FUNCION#####
-    #fix_chain('test_file.txt')
-    
-    #####TEST READ ATOM FUNCTION#####
-    #f = open('test_file.txt', 'r')
-    #Lines = f.readlines()
-    #for line in Lines:
-    #    if line[:1] != '#' and line[6:7] == '|':
-    #        print(read_atom(line))
-              
-    #####TEST INTERACTIONS TAKEN FROM FLEXAID FILE#####
-    #interact1 = interactions('MC_st0r5.2_6.dat', 7, 14)
-    #interact2 = interactions('MC_st0r5.2_6.dat', 7, 18)
-    #print(interact1*interact2)
-    
-    #####TEST ATOM TYPE NUMBERS TAKEN FROM FLEXAID FILE#####
-    #print (atomtype_num('AMINO.def', 'THR', 'OG1'))
-    
-    #####TEST ITEMS ON THE MATRIX#####
-    #print(matrix.loc['PHE338F', 'THR27D'])
-    #matrix.loc['PHE338F', 'THR27D'] += 1
-    #print (matrix)
     
     return ()
     
