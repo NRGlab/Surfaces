b3c76c88f051b050eeb11c302e03e85d4ded9bee
nataliateruel <56839341+nataliateruel@users.noreply.github.com>
2023-04-17 16:07:07 -0400

    Update surface_cont_res.py
    Corrected read_surface

--- surface_cont_res.py
+++ surface_cont_res.py
@@ -85,7 +85,7 @@ def read_atom(line):
     return (atnum,attype,resnum,res,chain)
 
 def read_surface(line):
-    surf = (float(line[-6:-1]))
+    surf = (float(line[-12:-6]))
     return (surf)
 
 def read_interactions(file, matrix, chains, sele_res, all_res, def_file, dat_file, atom_numbers):
